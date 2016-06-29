da6581a670a23cd8d5bb7ee01ca66df291cb9d07
Minh Nguyen <minhnguyen_1302@hotmail.com>
2016-06-29 09:07:14 -0700

    Commit bootstraping material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {MdToolbar} from '@angular2-material/toolbar';
+import {MdButton} from '@angular2-material/button';
+import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [MdToolbar, MdButton, MdIcon],
+  providers: [MdIconRegistry],
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Welcome to Angular 2 with Material';
 }
